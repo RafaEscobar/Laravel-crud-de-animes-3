f3a00530989e1486138004979793e59e68388e96
RafaEscobar <Rafael.EG.code@gmail.com>
2023-10-30 22:49:59 -0600

    Agregando emptyState a listado de animes

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -17,10 +17,20 @@
             </div>
         </div>
         <div class="w-8/12 p-8 flex flex-wrap">
-            @foreach (Auth()->user()->animes as $anime)
-                <x-card-anime-list :id="$anime->id" :title="$anime->name" :anime_portada_path="$anime->anime_portada_path" />
-            @endforeach
-                
+            @if ( count(Auth()->user()->animes) > 0 )
+                @foreach (Auth()->user()->animes as $anime)
+                    <x-card-anime-list :id="$anime->id" :title="$anime->name" :anime_portada_path="$anime->anime_portada_path" />
+                @endforeach
+            @else
+                <div class="flex flex-col justify-center items-center w-full">
+                    <div class="mb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                          </svg>                          
+                    </div>
+                    <p class="font-semibold text-xl">No cuentas con animes para mostrar</p>
+                </div>
+            @endif
         </div>
     </div>
 </x-app-layout>
