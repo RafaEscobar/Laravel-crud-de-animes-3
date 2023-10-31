c0db0103ab1abdb2b8c2191dfb68890ed229f051
RafaEscobar <Rafael.EG.code@gmail.com>
2023-10-30 19:49:39 -0600

    Merge branch r 'addPendingAnime'

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Anime;
 use App\Models\AnimePendingUser;
 use Illuminate\Http\Request;
-
+ 
 class DashboardController extends Controller
 {
     public function index(Request $request) 
