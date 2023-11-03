661259d3167fd008456a84fb3c6f8594065e7a35
RafaEscobar <Rafael.EG.code@gmail.com>
2023-11-02 18:39:11 -0600

    Detalle del usuario

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,20 +2,20 @@
     <x-slot name="header">
         <h2>Detalle del usuario</h2>
     </x-slot>
-    <div class="bg-red-500">
-        <div class="w-8/12">
+    <div class="flex justify-center">
+        <div class="w-4/12 mt-8">
             <div class="flex justify-center">
-              <img src="https://cdn-icons-png.flaticon.com/512/6073/6073873.png" alt="" srcset="" class="w-60 h-60">
+              <img src="{{Storage::url($user->profile_photo_path)}}" alt="" srcset="" class="w-60 h-60 rounded-full">
             </div>
             <hr class="mt-4 mb-2 border-black">
             <div class="ml-4">
               <span class="block mb-2">
                 <span class="text-xl font-semibold">Nombre:  </span>
-                <span class="text-lg">Rafael</span>
+                <span class="text-xl">{{$user->name}}</span>
               </span>
               <span class="black">
                 <span class="text-xl font-semibold">Correo electrónico:  </span>
-                <span class="text-lg">rafa@gmail.com</span>
+                <span class="text-xl">{{$user->email}}</span>
               </span>
             </div>
         </div>
