37bc43577bd33af7d619f1e69aac0d003682e394
RafaEscobar <Rafael.EG.code@gmail.com>
2023-11-03 20:27:35 -0600

    Cambio a form de partida para formulario

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,11 @@
     <x-slot name="header">
         <h2>Crear un nuevo usuario</h2>
     </x-slot>
-    @include('users.create')
+    <div class="flex justify-center ">
+        <div class="mt-6 w-6/12 bg-white p-6 rounded-xl mb-8">
+            {{ Form::open(['route' => 'user.store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) }}
+                @include('users.form')
+            {{ Form::close() }}
+        </div>
+    </div>
 </x-app-layout>
